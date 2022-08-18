44ed5d56220baac4487ca531830670730b35400c
Alexander Wrede <alexanderwrede.wr@gmail.com>
2022-08-18 12:14:03 +0200

    adapt to v6, add react native

---
+++ src/chapters/react_native/ReactNative.tsx
@@ -0,0 +1,74 @@
+/* eslint-disable react/no-unescaped-entities */
+import React from 'react';
+import Chapter, { GenericChapterProps } from '../../components/helper/Chapter';
+import Code from '../../components/helper/Code';
+import Slide from '../../components/reveal/Slide';
+
+const ReactNativeChapter: React.FC<GenericChapterProps> = (props: GenericChapterProps) => {
+  return (
+    <Chapter
+      {...props}
+      subtitle={
+        <blockquote>
+          &quot;Just like React but for mobile devices
+          &quot;
+        </blockquote>
+      }
+    >
+      <Slide>
+        <h2>Basics</h2>
+        <p className='fragment' style={{ fontSize: '28px', fontStyle: 'italic' }}>
+          React Native is a framework for building mobile apps with React. It is
+          based on React, and uses the same components, but with some changes to
+          make it work on native platforms like Android and iOS.
+        </p>
+      </Slide>
+      <Slide>
+        <h2>Components</h2>
+        <p className='fragment' style={{ fontSize: '28px', fontStyle: 'italic' }}>
+          Instead of using HTML tags, React Native uses components.
+        </p>
+        <Code className='fragment'>{`<div></div> -> <View></View>`}</Code>
+        <Code className='fragment'>{`<p></p> -> <Text></Text>`}</Code>
+      </Slide>
+      <Slide>
+        <h2>Logic</h2>
+        <p className='fragment' style={{ fontSize: '28px', fontStyle: 'italic' }}>
+          The logic can stay the same. We may need to adapt image uploads or other logic to use
+          native components from React Native.
+        </p>
+        <p className='fragment' style={{ fontSize: '28px', fontStyle: 'italic' }}>
+          When we want to add a React Native application to an existing React application, we can use a mono-repo approach.
+          A mono-repo contains the shared code aswell as the code for the native elements.
+        </p>
+      </Slide>
+      <Slide>
+        <h2>How to start</h2>
+        <p className='fragment' style={{ fontSize: '28px', fontStyle: 'italic' }}>
+          We can simply add the imports for react native and add them to our repo and then restructure the code.
+        </p>
+        <p className='fragment' style={{ fontSize: '28px', fontStyle: 'italic' }}>
+          We could also use expo, which is a platform for building native apps more easily as it also includes
+          approaches to directly test on iOS and Android Emulators and devices.
+        </p>
+      </Slide>
+
+      <Slide>
+        <h2>Further Reads</h2>
+        <ul>
+          <li>
+            <a href='https://reactnative.dev/docs/environment-setup'>React Native</a>
+          </li>
+          <li>
+            <a href='https://docs.expo.dev/get-started/installation/'>Expo</a>
+          </li>
+        </ul>
+      </Slide>
+      <Slide>
+        <h2>💪 Exercise</h2>
+      </Slide>
+    </Chapter>
+  );
+};
+
+export default ReactNativeChapter;
