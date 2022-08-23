45f832995ac623842d4a6c0d55b133f10dc6bcfb
Ludwig Hubert (Capgemini) <ludwig.hubert@capgemini.com>
2022-08-23 07:51:04 +0200

    Add chapter title to all slides

--- src/components/helper/Chapter.tsx
+++ src/components/helper/Chapter.tsx
@@ -12,12 +12,27 @@ interface ChapterProps extends GenericChapterProps {
 }
 
 const Chapter: React.FC<ChapterProps> = (props: ChapterProps) => {
+  const chapterTitle = `${props.index}. ${props.title}`;
   return (
     <Slide id={'chapter' + props.index}>
+      {/* Chapter title that is shown on all slides */}
+      <div
+        style={{
+          position: 'absolute',
+          right: 0,
+          writingMode: 'vertical-lr',
+          height: '100%',
+          opacity: 0.3
+        }}
+      >
+        {chapterTitle}
+      </div>
+      {/* First slide in chapter */}
       <Slide isMain>
-        <h2>{`${props.index}. ${props.title}`}</h2>
+        <h2>{chapterTitle}</h2>
         {props.subtitle}
       </Slide>
+      {/* All other chapter slides */}
       {props.children}
     </Slide>
   );
